ea21f53ebf9f0dca240800df8fb24930c3d72005
kushagra10bansal <115881616+kushagra10bansal@users.noreply.github.com>
2026-07-14 13:10:49 +0530

    feat: Add GitHub Actions workflow for ETL automation

--- src/load.py
+++ src/load.py
@@ -40,3 +40,8 @@ def upload_to_s3(file_path, bucket_name):
     )
 
     logger.info(f"Uploaded {file_name} to {bucket_name}")
+
+s3 = boto3.client(
+    "s3",
+    region_name=os.getenv("AWS_REGION")
+)
